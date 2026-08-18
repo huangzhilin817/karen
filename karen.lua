113a2e1da471f4e01ce6b222ae2d7024b6a71403
huangzhilin817 <2784977519@qq.com>
2026-08-18 15:43:45 +0800

    Update karen.lua

--- karen.lua
+++ karen.lua
@@ -1,1 +1,14 @@
-提示（“OK”）
+提示（“OK”）local packageName = activity.getPackageName()
+if packageName ~= "com.baoming" then
+    local builder = AlertDialog.Builder(activity)
+    builder.setTitle("验证失败")
+    builder.setMessage("当前应用包名不正确，即将退出。")
+    builder.setCancelable(false)
+    builder.setPositiveButton("确定", {onClick = function()
+        activity.finish()
+        System.exit(0)
+        Process.killProcess(Process.myPid())
+    end})
+    builder.show()
+    return
+end
