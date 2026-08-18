09b1d65fb92345d7806be71363ccba01150be696
huangzhilin817 <2784977519@qq.com>
2026-08-18 15:21:51 +0800

    Change package name check from 'com.baoming' to 'com.karen'

---
+++ karen.lua
@@ -0,0 +1,14 @@
+local packageName = activity.getPackageName()
+if packageName ~= "com.karen" then
+    local builder = AlertDialog.Builder(activity)
+    builder.setTitle("验证失败")
+    builder.setMessage("当前应用包名不正确，即将退出。")
+    builder.setCancelable(false)
+    builder.setPositiveButton("确定", {onClick = function()
+        activity.finish()
+        System.exit(0)
+        Process.killProcess(Process.myPid())
+    end})
+    builder.show()
+    return
+end
