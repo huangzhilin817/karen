44897ee3f40b83de51599b5bf20582ee7af8ccab
huangzhilin817 <2784977519@qq.com>
2026-08-18 15:39:07 +0800

    Remove package validation and exit dialog
    Removed package name validation and alert dialog.

--- karen.lua
+++ karen.lua
@@ -1,14 +1,1 @@
-local packageName = activity.getPackageName()
-if packageName ~= "com.karen" then
-    local builder = AlertDialog.Builder(activity)
-    builder.setTitle("验败失败")
-    builder.setMessage("当前应用包名不正确，即将退出。")
-    builder.setCancelable(false)
-    builder.setPositiveButton("确定", {onClick = function()
-        activity.finish()
-        System.exit(0)
-        Process.killProcess(Process.myPid())
-    end})
-    builder.show()
-    return
-end
+提示（“OK”）
